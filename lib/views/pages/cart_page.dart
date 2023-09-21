0969df9419c27e12f9cb72da8cbce4ad1fcf3627
mecodeur <mehdi.el2023@icloud.com>
2023-09-21 16:23:27 +0100

    List Items in My Cart Page : don't forget to solve Total Amount Issue

--- lib/views/pages/cart_page.dart
+++ lib/views/pages/cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecommerce_bloc/controllers/auth_bloc/auth_bloc.dart';
 import 'package:flutter_ecommerce_bloc/core/utils/app_router.dart';
-import 'package:flutter_ecommerce_bloc/models/product.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../models/add_to_cart_model.dart';
@@ -17,6 +18,7 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   double totalAmount = 0;
 
+  /*
   @override
   void didChangeDependencies() async {
     //final myProducts = await Provider.of<Database>(context).myProductCart().first;
@@ -26,82 +28,101 @@ class _CartPageState extends State<CartPage> {
         totalAmount += element.price;
       });
     });
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
-    final cartItems = dummyAddToCartModel;
-
+    final authBloc = BlocProvider.of<AuthBloc>(context);
     return SafeArea(
       child: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const SizedBox.shrink(),
-                  IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-                ],
-              ),
-              const SizedBox(height: 16.0),
-              Text(
-                'My Cart',
-                style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+          child: StreamBuilder<List<AddToCartModel>>(
+              stream: authBloc.database.myProductCart(),
+              builder: (context, snapshot) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const SizedBox.shrink(),
+                        IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.search)),
+                      ],
                     ),
-              ),
-              const SizedBox(height: 16.0),
-              if (cartItems == null || cartItems.isEmpty)
-                Center(
-                  child: Text(
-                    'No Data Available!',
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                ),
-              if (cartItems != null && cartItems.isNotEmpty)
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: cartItems.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final cartItem = cartItems[index];
-                    return CartListItem(
-                      cartItem: cartItem,
-                    );
-                  },
-                ),
-              const SizedBox(height: 24.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Total Amount:',
-                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                          color: Colors.grey,
+                    const SizedBox(height: 16.0),
+                    Text(
+                      'My Cart',
+                      style:
+                          Theme.of(context).textTheme.headlineMedium!.copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    StreamBuilder<List<AddToCartModel>>(
+                        stream: authBloc.database.myProductCart(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.active) {
+                            final List<AddToCartModel>? cartItems =
+                                snapshot.data;
+                            if (cartItems == null || cartItems.isEmpty) {
+                              return Center(
+                                child: Text(
+                                  'No Data Available!',
+                                  style:
+                                      Theme.of(context).textTheme.titleMedium,
+                                ),
+                              );
+                            }
+                            return ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: cartItems.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                final cartItem = cartItems[index];
+                                return CartListItem(
+                                  cartItem: cartItem,
+                                );
+                              },
+                            );
+                          }
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }),
+                    const SizedBox(height: 24.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Total Amount:',
+                          style:
+                              Theme.of(context).textTheme.titleMedium!.copyWith(
+                                    color: Colors.grey,
+                                  ),
                         ),
-                  ),
-                  Text(
-                    '${totalAmount.toString().substring(0, 5)}\$',
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 32.0),
-              MainButton(
-                text: 'Checkout',
-                onTap: () {
-                  GoRouter.of(context).push(AppRouter.kCheckoutRoute);
-                  //Navigator.of(context, rootNavigator: true).pushNamed(AppRoutes.checkoutPageRoute, arguments: database
-                },
-                hasCircularBorder: true,
-              ),
-              const SizedBox(height: 32.0),
-            ],
-          ),
+                        Text(
+                          '190\$',
+                          // '${totalAmount.toString().substring(0, 5)}\$',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 32.0),
+                    MainButton(
+                      text: 'Checkout',
+                      onTap: () {
+                        GoRouter.of(context).push(AppRouter.kCheckoutRoute);
+                        //Navigator.of(context, rootNavigator: true).pushNamed(AppRoutes.checkoutPageRoute, arguments: database
+                      },
+                      hasCircularBorder: true,
+                    ),
+                    const SizedBox(height: 32.0),
+                  ],
+                );
+              }),
         ),
       ),
     );
